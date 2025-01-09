ef708b550cf519792f9f7aa502f951319ab07f42
giovanni-arduini <gionnibigudd@gmail.com>
2025-01-09 15:13:55 +0100

    ADD BrowserRouter and Routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import DefaultLayout from "./layouts/DefaultLayout.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import MoviePage from "./pages/MoviePage.jsx";
 
 function App() {
-  return <></>;
+  <BrowserRouter>
+    <Routes>
+      <Route element={<DefaultLayout />}>
+        <Route path="/" element={HomePage} />
+        <Route path="/movies/:id" element={MoviePage} />
+      </Route>
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
