e4299effb99a999019ee4543370502894e6bd372
giovanni-arduini <gionnibigudd@gmail.com>
2025-01-09 15:08:14 +0100

    ADD MoviePage page component

---
+++ src/pages/MoviePage.jsx
@@ -0,0 +1,18 @@
+function MoviePage() {
+  return (
+    <>
+      <section>
+        <div className="container">
+          <h1>Titolo Film</h1>
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <h1>Descrizione e info</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default MoviePage;
