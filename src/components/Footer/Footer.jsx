45846c1108b596ca2da8de1c107c0646de9e86a9
giovanni-arduini <gionnibigudd@gmail.com>
2025-01-09 15:10:26 +0100

    ADD Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div>Questo è il footer</div>
+        <Link to="/" />
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
