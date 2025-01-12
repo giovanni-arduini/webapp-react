8c57ea1ec40ce7515298829f556748be74ed1803
giovanni-arduini <gionnibigudd@gmail.com>
2025-01-12 12:02:12 +0100

    ADD hover effect on Card

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -5,13 +5,17 @@ function MovieCard({ movie }) {
   const { id, title, director, image, abstract } = movie;
 
   return (
-    <div className=" col-3 gx-5 ">
+    <div className="col-3 gx-5">
       <div className="card h-100 text-white position-relative">
-        <img src={image} className="card-img object-fit-cover h-100" />
-        <div className="card-img-overlay p-0 ">
-          <div className={("card-body", styles.cardBody)}>
+        <img
+          src={image}
+          className="card-img object-fit-cover h-100"
+          alt={title}
+        />
+        <div className="card-img-overlay p-0">
+          <div className={`${styles.cardBody} card-body`}>
             <h5 className="card-title">{title}</h5>
-            <h6 className="card_subtite">{director}</h6>
+            <h6 className="card-subtitle">{director}</h6>
             <p className="card-text mb-3">{abstract}</p>
             <Link className={styles.cardLink} to={`/movies/${id}`}>
               Scopri il film
