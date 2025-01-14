7b1e6c0a776421f6ce14190abdbab8815673791d
giovanni-arduini <gionnibigudd@gmail.com>
2025-01-14 16:01:42 +0100

    ADD About page dummy content

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,18 @@
 function AboutPage() {
-  return <div>Ciao mamma!</div>;
+  return (
+    <div className="container main">
+      <h1 className="">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+        minus eius quas animi illum suscipit facilis unde reprehenderit! Eaque,
+        illo!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+        similique porro quae est laudantium.
+      </p>
+    </div>
+  );
 }
 
 export default AboutPage;
