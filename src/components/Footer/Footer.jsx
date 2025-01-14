db073e6ebaef14fe5e8dbb551ea7941b79bd3f2f
giovanni-arduini <gionnibigudd@gmail.com>
2025-01-14 15:47:36 +0100

    ADD footer link style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,18 +13,21 @@ function Footer() {
           commodi culpa recusandae!
         </div>
         <div className="d-flex justify-content-center">
-          <Link to="/" className="mx-3  link-underline-opacity-0 link-light">
+          <Link
+            to="/"
+            className="mx-3  link-underline-opacity-0 link-light link-opacity-75 link-opacity-100-hover"
+          >
             Home
           </Link>
           <Link
             to="/about"
-            className="mx-3 link-underline-opacity-0 link-light"
+            className="mx-3 link-underline-opacity-0 link-light link-opacity-75 link-opacity-100-hover"
           >
             About
           </Link>
           <Link
             to="/contacts"
-            className="mx-3 link-underline-opacity-0 link-light"
+            className="mx-3 link-underline-opacity-0 link-light link-opacity-75 link-opacity-100-hover"
           >
             Contacts
           </Link>
