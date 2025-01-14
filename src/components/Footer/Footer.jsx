84f76983875cb7182d0604bea2b15ab126641a4e
giovanni-arduini <gionnibigudd@gmail.com>
2025-01-14 15:27:30 +0100

    ADD Footer style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,31 @@ import styles from "./Footer.module.css";
 function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        <div>Questo è il footer</div>
-        <Link to="/" />
+      <footer
+        className={`${styles.footer} d-flex flex-column justify-content-center align-items-center`}
+      >
+        <div className="mb-2 mx-5 text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero itaque
+          blanditiis animi asperiores praesentium molestiae perferendis minus
+          commodi culpa recusandae!
+        </div>
+        <div className="d-flex justify-content-center">
+          <Link to="/" className="mx-3  link-underline-opacity-0 link-light">
+            Home
+          </Link>
+          <Link
+            to="/about"
+            className="mx-3 link-underline-opacity-0 link-light"
+          >
+            About
+          </Link>
+          <Link
+            to="/contacts"
+            className="mx-3 link-underline-opacity-0 link-light"
+          >
+            Contacts
+          </Link>
+        </div>
       </footer>
     </>
   );
